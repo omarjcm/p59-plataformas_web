0effbbc95737011be7bd2a6d89163457525876f7
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-11-22 19:38:54 -0500

    Javascript DOM
    Uso de los eventos del mouse.

---
+++ code/javascript-dom/taller_07/script.js
@@ -0,0 +1,28 @@
+var offset = [0,0]
+var divCajita = document.getElementById('cajita')
+var esPresionada = false
+
+divCajita.addEventListener('mousedown', function(e) {
+    esPresionada = true
+    console.log('[mousedown]')
+    console.log('offsetLeft: ' + divCajita.offsetLeft + ' - CoordX: ' + e.clientX)
+    console.log('offsetTop: ' + divCajita.offsetTop + ' - CoordY: ' + e.clientY)
+
+    offset = [
+        divCajita.offsetLeft - e.clientX,
+        divCajita.offsetTop - e.clientY
+    ]
+}, true)
+
+divCajita.addEventListener('mouseup', function() {
+    esPresionada = false
+}, true)
+
+divCajita.addEventListener('mousemove', function(e) {
+    console.log('[mousemove]')
+
+    if (esPresionada) {
+        divCajita.style.left = (e.clientX + offset[0]) + 'px'
+        divCajita.style.top = (e.clientY + offset[1]) + 'px'
+    }
+}, true)
