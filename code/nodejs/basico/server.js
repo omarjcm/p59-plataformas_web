3ca49d05f33d8b2a8f04199c5acb493ec9110761
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-27 21:09:46 -0500

    Creacion de un directorio con los archivos para la aplicacion cliente

--- code/nodejs/basico/server.js
+++ code/nodejs/basico/server.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const response = require('./network/response')
+
+const router = express.Router()
 
 let app = express()
 
-app.use('/', function(req, res) {
-    res.send('Hola mundo')
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded({extended:false}) )
+app.use( router )
+
+router.get('/mensaje', function(req, res) {
+    response.success(req, res, 'Hola desde GET')
 })
+
+router.post('/mensaje', function(req, res) {
+    response.success(req, res, 'Hola desde POST')
+})
+
+router.delete('/mensaje', function(req, res) {
+    response.success(req, res, 'Hola desde DELETE')
+})
+
+app.use('/', express.static('public'))
 
 let puerto = 8000
 app.listen(puerto)
